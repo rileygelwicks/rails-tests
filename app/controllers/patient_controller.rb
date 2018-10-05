bd0557f95207ea16f89efcf277a84103b685bfdf
Riley Gelwicks <riley.gelwicks@gmail.com>
2018-10-04 21:42:19 -0700

    Adds patient#transfer.

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -2,4 +2,8 @@ class PatientController < ApplicationController
   def show
     @patient = Patient.find(params[:id])
   end
+
+  def transfer
+    @patient = Patient.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "facility#index"
   resources :facility, only: [:show, :index] do
-    resources :patient, only: [:show]
+    resources :patient, only: [:show] do
+      member do
+        get 'transfer'
+      end
+    end
   end
 end
