7dd0da4902f60a2b8596b474462f40899b9dc640
Riley Gelwicks <riley.gelwicks@gmail.com>
2018-10-04 22:27:01 -0700

    Updates tests

--- test/controllers/patient_controller_test.rb
+++ test/controllers/patient_controller_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class PatientControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get patient_show_url
+
+    get facility_patient_url(facilities.first, patients.first)
+    assert_response :success
+  end
+
+  test "transfer member action" do
+    get transfer_facility_patient_url(facilities.first, patients.first)
     assert_response :success
   end
 
--- test/models/facility_test.rb
+++ test/models/facility_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class FacilityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "facilities should have patients" do
+    assert facilities.first.patients.count > 0, "First facility should have more than 1 patient"
+    assert facilities.second.patients.count > 0
+  end
 end
